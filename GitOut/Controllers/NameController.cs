e4c9c4fbc1309f39ba36e419748b094d32cb0486
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-11-03 23:35:22 -0500

    Add name controller

---
+++ GitOut/Controllers/NameController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<NameController> _logger;
+
+        public NameController(ILogger<NameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var personName = new Name { PersonName = "Dylan Mullarkey" };
+            return Ok(personName);
+        }
+    }
+}
--- GitOut/Controllers/WeatherForecastController.cs
+++ GitOut/Controllers/WeatherForecastController.cs
@@ -18,16 +18,11 @@ namespace GitOut.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet(Name = "GetName")]
+
+        public string Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Dylan Mullarkey";
         }
     }
 }
